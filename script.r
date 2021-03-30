dc80a0257891938b59b61e19b6453bd92a885b09
João Pedro <joaopedro@staffordcp.com>
2021-03-30 10:17:48 -0300

    Added Fundraising Chart code

--- script.r
+++ script.r
@@ -9,10 +9,28 @@ libraryRequireInstall("hrbrthemes")
 ####################################################
 
 ################### Actual code ####################
-g = qplot(`Petal.Length`, data = iris, fill = `Species`, main = Sys.time());
+ # The dataset is provided in the gapminder library
+ 
+ Values$CloseTDdate = as.Date(dataset$CloseTDdate)
+ 
+ data = Values
+ 
+ # Interactive version
+g = data %>%
+   
+   # prepare text for tooltip
+   mutate(text = paste("Name: ", Name, "\nValue: ", HighLowAvg, "Close Date/TD: ", CloseTDdate, sep="")) %>%
+   
+   # Classic ggplot
+   ggplot( aes(x=CloseTDdate, y=Priority, size = HighLowAvg, color = Priority, text=text)) +
+     geom_point(alpha=0.7) +
+     scale_size(range = c(1.5, 30), name="Value") +
+     scale_color_viridis(discrete=FALSE, guide=FALSE) +
+     theme_ipsum() +
+     theme(legend.position="none")
 ####################################################
 
 ############# Create and save widget ###############
-p = ggplotly(g);
+p = ggplotly(g, tooltip=text);
 internalSaveWidget(p, 'out.html');
 ####################################################
